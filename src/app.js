e1549ae6a48f2e1c4cd03707af922d3612074cfa
Suchismita Chakraborty <suchismita.chakraborty@ericsson.com>
2021-01-30 13:11:49 +0530

    Set up app for Heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ console.log(__dirname);
 console.log(__filename);
 // console.log(path.join(__dirname, "../public"));
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, "../public");
@@ -92,6 +93,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port : " + port);
 });
